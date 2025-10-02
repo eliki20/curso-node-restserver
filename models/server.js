1f5aaed43b66d5e6c49558d9950ddd9133acb5d5
eliki20 <eliana.terrones1@unmsm.edu.pe>
2025-10-01 23:47:26 -0500

    Update server.js

--- models/server.js
+++ models/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 class Server {
     constructor() {
         this.app = express();
-        this.port = process.env.PORT || 3000;
+        this.port = process.env.PORT;
         this.usuariosPath = '/api/usuarios';
         //Middelware
         this.middlewares();
