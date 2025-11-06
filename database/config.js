b23531c0cf4f72cecb2c4d6986b82c88774e83e5
Eliana <eliana.terrones1@unmsm.edu.pe>
2025-11-06 01:17:57 -0500

    Fin Seccion 9

--- database/config.js
+++ database/config.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
     try {
         await mongoose.connect(process.env.MONGO_CNN, {
-            //useNewUrlParser: true,
-            //useUnifiedTopology: true
+            useNewUrlParser: true,
+            useUnifiedTopology: true
             //useCreateIndex: true,
             //useFindAndModify: false    
         });
